a55f5944395595823ad70331ff570c70a508f5ae
Ameyanagi <ryuichi@ameyanagi.com>
2024-01-25 09:45:35 -0500

    add spdist+MAE as a metric

--- xasalign/xasalign.py
+++ xasalign/xasalign.py
@@ -46,33 +46,40 @@ def _residue(
     # print(
     #     spdist.squared_spdist(energy_grid, spectrum_y, ref_spectrum_x, ref_spectrum_y)
     # )
-    return spdist.squared_spdist(
+    return spdist.spdist(
         energy_grid, spectrum_y, ref_spectrum_x, ref_spectrum_y
+    ) + np.mean(
+        np.abs(spectrum_y - np.interp(energy_grid, ref_spectrum_x, ref_spectrum_y))
     )
 
 
 def _residue_MSE(
     p: list,
+    energy_grid: np.ndarray,
     spectrum_x: np.ndarray,
     spectrum_y: np.ndarray,
     ref_spectrum_x: np.ndarray,
     ref_spectrum_y: np.ndarray,
     fit_range: list | None = None,
 ):
+    # print(len(spectrum_y))
+    spectrum_y = np.interp(energy_grid, spectrum_x + p[0], spectrum_y * p[1])
+
     if fit_range:
-        index = np.where(
-            (spectrum_x + p[0] >= fit_range[0]) & (spectrum_x + p[0] <= fit_range[1])
-        )
-        spectrum_x = spectrum_x[index]
+        index = np.where((energy_grid >= fit_range[0]) & (energy_grid <= fit_range[1]))
+        energy_grid = energy_grid[index]
         spectrum_y = spectrum_y[index]
 
-    return np.sum(
-        (
-            spectrum_y
-            - np.interp(spectrum_x, ref_spectrum_x - p[0], ref_spectrum_y / p[1])
-        )
-        ** 2
+    return np.mean(
+        (spectrum_y - np.interp(energy_grid, ref_spectrum_x, ref_spectrum_y)) ** 2
     )
+    # return np.sum(
+    #     (
+    #         spectrum_y
+    #         - np.interp(spectrum_x, ref_spectrum_x - p[0], ref_spectrum_y / p[1])
+    #     )
+    #     ** 2
+    # )
 
 
 def calc_shift_scale(
@@ -130,7 +137,13 @@ def calc_shift_scale(
 
 
 def calc_shift_scale_MSE(
-    spectrum_x, spectrum_y, ref_spectrum_x, ref_spectrum_y, fit_range=None, max_shift=20
+    energy_grid,
+    spectrum_x,
+    spectrum_y,
+    ref_spectrum_x,
+    ref_spectrum_y,
+    fit_range=None,
+    max_shift=20,
 ):
     if fit_range:
         index = np.where(
@@ -143,7 +156,13 @@ def calc_shift_scale_MSE(
     p0 = [0, 1]
 
     residue = lambda p: _residue_MSE(
-        p, spectrum_x, spectrum_y, ref_spectrum_x, ref_spectrum_y, fit_range
+        p,
+        energy_grid,
+        spectrum_x,
+        spectrum_y,
+        ref_spectrum_x,
+        ref_spectrum_y,
+        fit_range,
     )
 
     results = least_squares(residue, p0)
@@ -165,7 +184,7 @@ def test():
     spectrum_y = theoretical_data[:, 3]
 
     spectrum_x = spectrum_x - 10
-    spectrum_y = spectrum_y / 1.1
+    spectrum_y = spectrum_y
     e0 = 11564
     fit_range = [e0 - 20, e0 + 80]
     energy_grid = np.linspace(fit_range[0], fit_range[1], 100)
@@ -181,12 +200,29 @@ def test():
     )
 
     print(shift, scale)
-    fig, ax = plt.subplots(1, 1, figsize=(3, 3))
+    fig, ax = plt.subplots(1, 1, figsize=(6, 6))
 
     ax.plot(ref_spectrum_x, ref_spectrum_y, label="Pt foil(exp)")
     ax.plot(spectrum_x, spectrum_y, label="Pt foil(feff)")
 
-    ax.plot(spectrum_x + shift, spectrum_y * scale, label="Pt foil(feff: scale spdist)")
+    ax.plot(
+        spectrum_x + shift,
+        spectrum_y * scale,
+        label="Pt foil(feff: scale spdist + MAE)",
+    )
+
+    shift, scale, loss = calc_shift_scale_MSE(
+        energy_grid,
+        spectrum_x,
+        spectrum_y,
+        ref_spectrum_x,
+        ref_spectrum_y,
+        fit_range=fit_range,
+        max_shift=20,
+    )
+
+    ax.plot(spectrum_x + shift, spectrum_y * scale, label="Pt foil(feff: scale MSE)")
+
     ax.set_xlim(*fit_range)
     ax.legend()
 
